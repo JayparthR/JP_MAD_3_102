cf1a82a6e42c4c58a29eec907536d6ecacfda0bd
student <student@gnu.ac.in>
2023-08-08 12:36:57 +0530

    prac3

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "JP_MAD_3_102"
+include(":app")
